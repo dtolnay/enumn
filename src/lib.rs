3684a607d9ac42e9e43f0f576ce675b924d90fb0
victorhowisers <161865211+victorhowisers@users.noreply.github.com>
2024-03-02 01:20:27 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
 //! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
 //! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
-//!
+//! sdf
 //! <br>
 //!
 //! Convert number to enum.
